395ccda6269d3b813369539895adbbe6d5a85da4
AnnaKozhokaru <rero56789@gmail.com>
2022-11-23 20:51:14 +0200

    [master]: Add classroom progress 23.11

--- src/test/java/TestChrome.java
+++ src/test/java/TestChrome.java
@@ -2,11 +2,14 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 
+import static lv.acodemy.constants.Generic.GOOGLE_URL;
+import static org.testng.Assert.assertEquals;
+
 public class TestChrome {
+
     ChromeDriver driver = new ChromeDriver();
 
     @AfterMethod
@@ -17,9 +20,9 @@ public class TestChrome {
 
     @Test
     public void chromeTest() {
-        driver.get("https://google.lv/?hl=en");
+        driver.get(GOOGLE_URL);
 
-        WebElement acceptButton = driver.findElement(By.xpath("//button//div[contains(text(),'Accept all')]"));
+        WebElement acceptButton = driver.findElement(By.xpath("//button//div[contains(text(), 'Accept all')]"));
         acceptButton.click();
 
         WebElement searchField = driver.findElement(By.name("q"));
@@ -27,6 +30,6 @@ public class TestChrome {
 
         searchField.sendKeys(Keys.ENTER);
 
-        Assert.assertEquals(driver.getTitle(), "acodemy - Google Search");
+        assertEquals(driver.getTitle(), "acodemy - Google Search");
     }
 }
